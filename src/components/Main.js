f586ec8a552b2dcea4a7782cb28921106ae37888
Danila Semenov <inflames147@gmail.com>
2023-05-11 16:03:53 +0300

    refactor: Добавьте императивные обработчики

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import avatar from '../images/profile.jpg';
 
+function handleEditAvatarClick() {
+  document.querySelector('.popup_avatar-edit').classList.add('popup__opened');
+}
+
+function handleEditProfileClick() {
+  document.querySelector('.popup_profile-edit').classList.add('popup__opened');
+}
+
+function handleAddPlaceClick() {
+  document.querySelector('.popup_card-add').classList.add('popup__opened');
+}
+
 function Main() {
   return (
     <main className='content'>
       <section className='profile profile-position'>
-        <div className='profile__avatar'>
+        <div className='profile__avatar' onClick={handleEditAvatarClick}>
           <img src={avatar} alt='фото профиля' className='profile__image' />
         </div>
         <div className='profile__info'>
           <div className='profile__container'>
             <h1 className='profile__name'>Семенов Данила</h1>
-            <button type='button' className='profile__edit'></button>
+            <button
+              type='button'
+              className='profile__edit'
+              onClick={handleEditProfileClick}
+            ></button>
           </div>
           <p className='profile__about'>Веб-разработчик</p>
         </div>
-        <button type='button' className='profile__add'></button>
+        <button
+          type='button'
+          className='profile__add'
+          onClick={handleAddPlaceClick}
+        ></button>
       </section>
       <section className='elements elements-position'>
         <ul className='elements__places'></ul>
